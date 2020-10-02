7e1db751ea00f691c9defa139ac3c90520a46f17
Ngay Vong <ngayvong5190@gmail.com>
2020-10-02 12:17:31 -0400

    Initial Commit

---
+++ PhotoHunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoHunt
+//
+//  Created by Ngay Vong on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
